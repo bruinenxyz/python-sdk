21139552c0dcb936c2f048d4e667915526a000d4
Tyler Dammann <tylerdammann@gmail.com>
2023-05-25 13:32:25 -0700

    Restructure SDK

--- bruinen/src/bruinen/langchain/github_test.py
+++ bruinen/src/bruinen/langchain/github_test.py
@@ -1,18 +1,17 @@
 import json
 from typing import List, Optional
 
-from pydantic import BaseModel, Field
-
 from langchain.base_language import BaseLanguageModel
 from langchain.callbacks.manager import AsyncCallbackManagerForToolRun, CallbackManagerForToolRun
 from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts.prompt import PromptTemplate
 from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
 
-from ..bruinen_api_client import AuthenticatedClient
-from ..bruinen_api_client.api.accounts import find_all_accounts_for_user
-from ..bruinen_api_client.models import ReturnedAccountDto
-from ..bruinen_api_client.types import Response
+from ..client import AuthenticatedClient
+from ..client.api.accounts import find_all_accounts_for_user
+from ..client.models import ReturnedAccountDto
+from ..client.types import Response
 
 # Import input models (there aren't any for this one)
 
@@ -92,8 +91,8 @@ class GithubGetReposTool(BaseTool):
             return "The user has not connected their Github account; you should try authenticating Github first."
         else:
             # User has authenticated Github, make the request
-            from ..bruinen_api_client.api.sources import github_controller_profile
-            from ..bruinen_api_client.models import GithubProfile
+            from ..client.api.sources import github_controller_profile
+            from ..client.models import GithubProfile
 
             response: Response[GithubProfile] = github_controller_profile.sync_detailed(
                 client=self.client, account_id=account_id
--- langchain_tools_example.py
+++ langchain_tools_example.py
@@ -9,8 +9,8 @@ bruinen_secret = os.getenv('BRUINEN_SECRET')
 from langchain.agents import initialize_agent
 from langchain.llms import OpenAI
 
-from bruinen.langchain.github_test import GithubGetReposTool
-from bruinen.client import AuthenticatedClient
+from bruinen.src.bruinen.langchain.github_test import GithubGetReposTool
+from bruinen.src.bruinen.client import AuthenticatedClient
 
 client = AuthenticatedClient(base_url='http://localhost:3000', token=bruinen_secret, prefix='', auth_header_name='X-API-Key')
 
